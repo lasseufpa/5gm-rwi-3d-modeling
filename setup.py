7fcbcd1e6294fac6524132b3893bd030525bca14
Pedro Batista <pedosb@gmail.com>
2018-01-24 13:28:55 -0300

    Setup file

---
+++ setup.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='rwiparsing',
+      version='1.0',
+      description='Parsing of Remcom Wireless InSite .p2m files',
+      author='LASSE',
+      author_email='pedosb@gmail.com',
+      url='https://gitlab.lasse.ufpa.br/software/python-machine-learning/rwi-parsing',
+      packages=['rwimodeling'],
+      requires=['numpy(>=1.14)']
+      )
