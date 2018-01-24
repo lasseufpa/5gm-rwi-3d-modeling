b8f6225406dc4af896c0f7b8c1842d370a3ed40f
Pedro Batista <pedosb@gmail.com>
2018-01-24 13:30:15 -0300

    project name was wrong

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@
 
 from distutils.core import setup
 
-setup(name='rwiparsing',
+setup(name='rwimodeling',
       version='1.0',
       description='Parsing of Remcom Wireless InSite .p2m files',
       author='LASSE',
